55586e0e7461212c8891399c8ac0692ab59c8eb3
nielsbosma <niels@bosmainteractive.se>
2025-10-03 15:28:00 +0200

    feat(server): add dynamic volume path based on environment
    Add FolderVolume with path set to "/app/data" in production,
    otherwise use default path.

--- crm-vc/Program.cs
+++ crm-vc/Program.cs
@@ -7,4 +7,5 @@ server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
 var chromeSettings = new ChromeSettings().UseTabs(preventDuplicates: true);
 server.UseChrome(chromeSettings);
+server.UseVolume(new FolderVolume(Ivy.Utils.IsProduction() ? "/app/data" : null));
 await server.RunAsync();
