9c5f8239089a7e65e1be322406168138f1964cf1
ymykhal <ymykhal@gmail.com>
2025-10-07 12:16:06 +0300

    Package version bugfix; variable renames
    - fixed package reference version
    - renamed variables in Date Time parser example for improved clarity

--- superpower/Apps/DateTimeParserView.cs
+++ superpower/Apps/DateTimeParserView.cs
@@ -6,31 +6,31 @@ namespace Superpower.Apps
     {
         public override object? Build()
         {
-            var dateState = UseState("2017-01-01 05:28:10");
+            var dateTextState = UseState("2017-01-01 05:28:10");
+            var resultDateValueState = UseState(DateTime.MinValue);
             var errorState = UseState<string>("");
             var resultState = UseState(false);
-            var resultValueState = UseState(DateTime.MinValue);
             var parsingState = UseState(false);
 
             var eventHandler = (Event<Button> e) =>
             {
                 errorState.Set("");
                 resultState.Set(false);
-                resultValueState.Set(DateTime.MinValue);
+                resultDateValueState.Set(DateTime.MinValue);
                 parsingState.Set(true);
 
-                if (!string.IsNullOrWhiteSpace(dateState.Value))
+                if (!string.IsNullOrWhiteSpace(dateTextState.Value))
                 {
                     try
                     {
-                        var resultDateTime = DateTimeTextParser.Parse(dateState.Value);
-                        resultValueState.Set(resultDateTime);
+                        var resultDateTime = DateTimeTextParser.Parse(dateTextState.Value);
+                        resultDateValueState.Set(resultDateTime);
                         resultState.Set(true);
                     }
                     catch (Exception ex)
                     {
                         resultState.Set(false);
-                        resultValueState.Set(DateTime.MinValue);
+                        resultDateValueState.Set(DateTime.MinValue);
                         errorState.Set($"Error occurred: {ex.Message}");
                     }
                 }
@@ -38,13 +38,13 @@ namespace Superpower.Apps
             };
 
             return Layout.Vertical().Gap(2).Padding(2)
-                | dateState.ToTextInput().WithLabel("Type a date and time string in ISO-8601 format")
+                | dateTextState.ToTextInput().WithLabel("Type a date and time string in ISO-8601 format")
                 | new Button("Parse", eventHandler).Loading(parsingState.Value)
                 | (errorState.Value.Length > 0 ? Text.Block(errorState.Value) : null)
                 | (resultState.Value ? new Card(
                     Layout.Vertical().Gap(1).Padding(1)
                     | Text.Block("Result:")
-                    | Text.P(resultValueState.Value.ToString())
+                    | Text.P(resultDateValueState.Value.ToString())
                 ).Width(300) : null)
                 ;
         }
