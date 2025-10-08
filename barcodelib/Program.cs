50d2d1c658b00ad7ed57911224042e821785534f
Artem Lazarchuk <131264474+ArtemLazarchuk@users.noreply.github.com>
2025-10-08 21:43:19 +0300

    Refactor BarcodeLib (#225)
    * feat: upload last version ivy 1.*
    
    * feat: rename files as an example
    
    * feat: reorganize BarcodeLib add two-card layout
    
    * feat: improve better UI
    
    * feat: create devcontainer for barcodelib
    
    * feat: add button "open GitHub in Codespaces"
    
    * feat: update README.md
    
    * feat: create devcontainer for Ivy-Examples
    
    * feat: rename name in devcotainer for Ivy Examples
    
    * feat: remove devcontainer for open repo with all example, and change UI
    
    * feat: update README.md

--- barcodelib/Program.cs
+++ barcodelib/Program.cs
@@ -1,4 +1,4 @@
-using BarcodelibExample.Apps;
+using BarcodeLibExample.Apps;
 using Ivy;
 using Ivy.Chrome;
 using System.Globalization;
@@ -13,8 +13,18 @@ server.UseHotReload();
 #endif
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
+var customHeader = Layout.Vertical().Gap(2).Align(Align.Center)
+    | new Html(@"
+        <div>
+          <a href=""https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fbarcodelib%2Fdevcontainer.json&location=EuropeWest"">
+            <img src=""https://github.com/codespaces/badge.svg"" alt=""Open BarcodeLib in Codespaces"" />
+          </a>
+        </div>
+      ")
+    | new Button("Source Code").Url("https://github.com/Ivy-Interactive/Ivy-Examples/tree/main/barcodelib").Icon(Icons.ExternalLink).Width(Size.Full());
 var chromeSettings = new ChromeSettings()
     .DefaultApp<BarcodeLibApp>()
-    .UseTabs(preventDuplicates: true);
+    .UseTabs(preventDuplicates: true)
+    .Header(customHeader);
 server.UseChrome(chromeSettings);
 await server.RunAsync();
