1ffa658e4f797a4b58a07628987f44677e01aa30
defymecobra <168521123+defymecobra@users.noreply.github.com>
2025-10-10 12:13:00 +0300

    create questpdf demo (#229)
    * feat: create questpdf demo
    
    * chore: fix build and devcontainer

---
+++ questpdf/Program.cs
@@ -0,0 +1,23 @@
+using QuestPdfDemo.Apps;
+
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+// Set QuestPDF license for non-production/community usage
+QuestPDF.Settings.License = LicenseType.Community;
+var server = new Server();
+#if DEBUG
+server.UseHotReload();
+#endif
+server.AddAppsFromAssembly();
+server.AddConnectionsFromAssembly();
+var customHeader = Layout.Vertical().Gap(2).Align(Align.Center)
+    | new Html(@"
+        <div>
+          <a href=""https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fquestpdf%2Fdevcontainer.json&location=EuropeWest"">
+            <img src=""https://github.com/codespaces/badge.svg"" alt=""Open QuestPDF in Codespaces"" />
+          </a>
+        </div>
+      ")
+    | new Button("Source Code").Url("https://github.com/Ivy-Interactive/Ivy-Examples/tree/main/questpdf").Icon(Icons.ExternalLink).Width(Ivy.Shared.Size.Full());
+var chromeSettings = new ChromeSettings().DefaultApp<QuestPdfApp>().UseTabs(preventDuplicates: true).Header(customHeader);
+server.UseChrome(chromeSettings);
+await server.RunAsync();
