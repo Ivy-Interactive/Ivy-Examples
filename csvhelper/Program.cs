735619f3e748e115215bd913a1ce9f6eaabad195
Artem <la.artem.ser@gmail.com>
2025-10-14 21:51:28 +0300

    feat: update Codespaces link for csvhelper, remove HTML and use embed instead

--- csvhelper/Program.cs
+++ csvhelper/Program.cs
@@ -5,14 +5,8 @@ server.UseHotReload();
 #endif
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
-var customHeader = Layout.Vertical().Gap(2).Align(Align.Center)
-    | new Html(@"
-        <div>
-          <a href=""https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fcsvhelper%2Fdevcontainer.json&location=EuropeWest"">
-            <img src=""https://github.com/codespaces/badge.svg"" alt=""Open CsvHelper in Codespaces"" />
-          </a>
-        </div>
-      ");
+var customHeader = Layout.Vertical().Gap(2)
+    |new Embed("https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fcsvhelper%2Fdevcontainer.json&location=EuropeWest");
 var chromeSettings = new ChromeSettings()
     .DefaultApp<CsvHelperApp>()
     .UseTabs(preventDuplicates: true)
