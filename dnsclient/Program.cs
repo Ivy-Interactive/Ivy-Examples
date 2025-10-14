d59ce5e0f11a705dde7a6c438923bd2bd1d9f81f
Artem <la.artem.ser@gmail.com>
2025-10-14 21:47:07 +0300

    feat: update Codespaces link for dnsclient, remove HTML and use embed instead

--- dnsclient/Program.cs
+++ dnsclient/Program.cs
@@ -13,14 +13,8 @@ server.AddConnectionsFromAssembly();
 
 server.Services.AddSingleton<ILookupClient>(i => new LookupClient());
 
-var customHeader = Layout.Vertical().Gap(2).Align(Align.Center)
-    | new Html(@"
-        <div>
-          <a href=""https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fdnsclient%2Fdevcontainer.json&location=EuropeWest"">
-            <img src=""https://github.com/codespaces/badge.svg"" alt=""Open DNSClient in Codespaces"" />
-          </a>
-        </div>
-      ");
+var customHeader = Layout.Vertical().Gap(2)
+    |new Embed("https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fdnsclient%2Fdevcontainer.json&location=EuropeWest");
 var chromeSettings = new ChromeSettings()
     .DefaultApp<DnsLookUpApp>()
     .UseTabs(preventDuplicates: true)
