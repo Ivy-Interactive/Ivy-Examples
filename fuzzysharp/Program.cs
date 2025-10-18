65b73a7b7d560a930954b497fa0aeedb4d6d7ebd
Artem <la.artem.ser@gmail.com>
2025-10-18 17:55:54 +0300

    feat: set FuzzySharpApp as default app

--- fuzzysharp/Program.cs
+++ fuzzysharp/Program.cs
@@ -1,3 +1,4 @@
+using FuzzySharpExample;
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 var server = new Server();
 #if DEBUG
@@ -5,6 +6,6 @@ server.UseHotReload();
 #endif
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
-var chromeSettings = new ChromeSettings().UseTabs(preventDuplicates: true);
+var chromeSettings = new ChromeSettings().DefaultApp<FuzzySharpApp>().UseTabs(preventDuplicates: true);
 server.UseChrome(chromeSettings);
 await server.RunAsync();
