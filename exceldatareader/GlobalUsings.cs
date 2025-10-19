34da15e7b69cb4f5a7f8abb0986b629c08083751
Artem Lazarchuk <131264474+ArtemLazarchuk@users.noreply.github.com>
2025-10-19 16:04:08 +0300

    Refactor ExcelDataReader (#257)
    * feat: rename files as an example
    
    * feat: set ExcelDataReaderApp as default app
    
    * feat: any file is rendered
    
    * feat: add new better logic for excelDataReader
    
    * feat: improved logic for Excel File Analyzer (left card)
    
    * feat: improved logic for File Analysis Results(right card)
    
    * feat: improve CSV support and fix null reference errors
    
    * feat: remove not needed files
    
    * feat: add  info about ivy and ExcelDataReader
    
    * feat: add devcontainer.json for exceldatareader
    
    * feat: add Codespaces link and use embed instead
    
    * feat: set Ivy last version 1.*
    
    * feat: set ivy version 1.0.121
    
    * feat: update README.md
    
    * feat: set last ivy version 1.*
    
    * fix: resolve DataTable namespace conflicts in closedxml project

--- exceldatareader/GlobalUsings.cs
+++ exceldatareader/GlobalUsings.cs
@@ -26,4 +26,4 @@ global using System.ComponentModel.DataAnnotations;
 global using System.Globalization;
 global using System.Reactive.Linq;
 global using ExcelDataReader;
-namespace exceldatareaderApp;
+namespace ExcelDataReaderExample;
