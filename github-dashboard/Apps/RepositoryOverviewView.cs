16df182c435d578f8041facd6e09eca89666ab53
rorychatt <pavel.rinne@gmail.com>
2025-10-23 14:24:26 +0200

    Fix UseEffect signature - remove refresh trigger dependency for now

--- github-dashboard/Apps/RepositoryOverviewView.cs
+++ github-dashboard/Apps/RepositoryOverviewView.cs
@@ -46,7 +46,7 @@ public class RepositoryOverviewView : ViewBase
                     isLoading.Set(false);
                 }
             });
-        }, _refreshTrigger);
+        });
 
         if (isLoading.Value)
         {
