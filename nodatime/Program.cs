14b2173496b08cabae0f9719f4e1017ead98ebe2
Artem <la.artem.ser@gmail.com>
2025-11-03 19:39:03 +0200

    feat: add custom header to ChromeSettings in NodaTime for enhanced user experience

--- nodatime/Program.cs
+++ nodatime/Program.cs
@@ -6,6 +6,11 @@ server.UseHotReload();
 #endif
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
-var chromeSettings = new ChromeSettings().DefaultApp<NodaTimeApp>().UseTabs(preventDuplicates: true);
+var customHeader = Layout.Vertical().Gap(2)
+    |new Embed("https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fnodatime%2Fdevcontainer.json&location=EuropeWest");
+var chromeSettings = new ChromeSettings()
+    .DefaultApp<NodaTimeApp>()
+    .UseTabs(preventDuplicates: true)
+    .Header(customHeader);
 server.UseChrome(chromeSettings);
 await server.RunAsync();
