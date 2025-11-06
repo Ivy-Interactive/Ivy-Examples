0fa9c5d7534b77ba29a8844835ea31f025b5bb05
Artem Lazarchuk <131264474+ArtemLazarchuk@users.noreply.github.com>
2025-11-06 14:30:38 +0200

    Refactor NodaTime (#282)
    * feat: rename files as an example
    
    * feat: improve better UI
    
    * feat: add info about Ivy-Framework and NodeTime
    
    * feat: add devcontainer.json for NodeTime
    
    * feat: remove SampleBase and CodeView classes from NodaTime example
    
    * feat: add custom header to ChromeSettings in NodaTime for enhanced user experience
    
    * feat: update README.md

--- nodatime/GlobalUsings.cs
+++ nodatime/GlobalUsings.cs
@@ -24,5 +24,8 @@ global using System.Collections.Immutable;
 global using System.ComponentModel.DataAnnotations;
 global using System.Globalization;
 global using System.Reactive.Linq;
+global using System.Reflection;
+global using NodaTime;
+global using NodaTime.Text;
 
-namespace IvyHelloWorld;
+namespace NodaTimeExample;
