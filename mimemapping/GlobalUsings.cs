5da97101e05c5c401535743751d8bd5ccab6a2ec
Artem <la.artem.ser@gmail.com>
2025-11-06 15:58:54 +0200

    feat(mimemapping): integrate new Ivy upload API for file handling and update file input logic

--- mimemapping/GlobalUsings.cs
+++ mimemapping/GlobalUsings.cs
@@ -8,6 +8,7 @@ global using Ivy.Core.Hooks;
 global using Ivy.Helpers;
 global using Ivy.Hooks;
 global using Ivy.Shared;
+global using Ivy.Services;
 global using Ivy.Views;
 global using Ivy.Views.Alerts;
 global using Ivy.Views.Blades;
