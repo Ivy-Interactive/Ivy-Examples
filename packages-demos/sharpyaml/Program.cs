4ef3ff818936e543424ef286a349530ba17bd194
Artem Lazarchuk <131264474+ArtemLazarchuk@users.noreply.github.com>
2025-11-10 21:56:31 +0200

    Refactor SharpYaml (#294)
    * feat: update default model in SharpYamlDemoApp to include nested objects and arrays for enhanced demonstration
    
    * feat: rename files as an example
    
    * feat: improve better UI
    
    * feat: add devcontainer.json for sharpyaml
    
    * feat: add footer section to SharpYamlApp with library information and credits
    
    * feat: add custom header to ChromeSettings in SharpYamlApp for enhanced user experience
    
    * feat: update README.md

--- packages-demos/sharpyaml/Program.cs
+++ packages-demos/sharpyaml/Program.cs
@@ -1,4 +1,4 @@
-using Sharpyaml.Apps;
+using SharpYamlExample;
 
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 var server = new Server();
@@ -7,6 +7,11 @@ server.UseHotReload();
 #endif
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
-var chromeSettings = new ChromeSettings().DefaultApp<SharpYamlDemoApp>().UseTabs(preventDuplicates: true);
+var customHeader = Layout.Vertical().Gap(2)
+    |new Embed("https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=Ivy-Interactive%2FIvy-Examples&machine=standardLinux32gb&devcontainer_path=.devcontainer%2Fsharpyaml%2Fdevcontainer.json&location=EuropeWest");
+var chromeSettings = new ChromeSettings()
+    .DefaultApp<SharpYamlApp>()
+    .UseTabs(preventDuplicates: true)
+    .Header(customHeader);
 server.UseChrome(chromeSettings);
 await server.RunAsync();
