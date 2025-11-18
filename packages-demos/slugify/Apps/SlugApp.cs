d81b5a7873cc51ae43ab8ea9cd5c2ba2eeb83f28
joshuauaua <joshjoelng@gmail.com>
2025-11-18 21:05:04 +0700

    Removed copilot suggestion to Collapse#hiteSpace as it is performed by default by the library so does not need to be defined (leads to an error)

--- packages-demos/slugify/Apps/SlugApp.cs
+++ packages-demos/slugify/Apps/SlugApp.cs
@@ -33,7 +33,6 @@ public class SlugApp : ViewBase
                     ForceLowerCase = ToBool(lowerCaseState.Value),
                     TrimWhitespace = ToBool(trimWhitespaceState.Value),
                     CollapseDashes = ToBool(collapseDashesState.Value),
-                    CollapseWhitespace = ToBool(collapseWhitespaceState.Value)
                 };
 
                 // Create SlugHelper instance with the configuration
