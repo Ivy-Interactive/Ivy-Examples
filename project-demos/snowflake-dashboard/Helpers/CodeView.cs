19c79eab43779019145c47cafa66e0ec7aca3fec
Artem <la.artem.ser@gmail.com>
2025-12-12 19:37:21 +0200

    refactor: streamline DashboardApp by removing credential verification logic and integrating configuration-based credential management

--- project-demos/snowflake-dashboard/Helpers/CodeView.cs
+++ project-demos/snowflake-dashboard/Helpers/CodeView.cs
@@ -1,23 +1,21 @@
 namespace SnowflakeDashboard.Helpers;
 
-public class CodeView(Type type) : ViewBase
+public class CodeView : ViewBase
 {
     public override object? Build()
     {
         var assembly = typeof(CodeView).Assembly;
         var resourceName = "SnowflakeDashboard.Apps.DashboardApp.cs";
-        string code;
-        using (Stream? stream = assembly.GetManifestResourceStream(resourceName))
+        
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream == null)
         {
-            if (stream == null)
-            {
-                return new Exception("Resource not found.");
-            }
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                code = reader.ReadToEnd();
-            }
+            return new Exception("Resource not found.");
         }
+        
+        using var reader = new StreamReader(stream);
+        var code = reader.ReadToEnd();
+        
         return new Code(code, Languages.Csharp).Width(Size.Fit()).Height(Size.Fit());
     }
 }
