c4963ee48e60f5df34e5556e1d12be946cdd42df
Artem <la.artem.ser@gmail.com>
2026-01-12 19:35:59 +0200

    refactor: implement null check and return Skeleton for chart values in crm-vc

--- project-demos/crm-vc/Apps/Views/DashboardApp.DailyDealCountByRoundLineChartView.cs
+++ project-demos/crm-vc/Apps/Views/DashboardApp.DailyDealCountByRoundLineChartView.cs
@@ -46,10 +46,10 @@ public class DailyDealCountByRoundLineChartView(DateTime startDate, DateTime end
             return card | new ErrorTeaserView(exception.Value);
         }
 
-        // if (chart.Value == null)
-        // {
-        //     return card | new Skeleton();
-        // }
+        if (chart.Value == null)
+        {
+            return card | new Skeleton();
+        }
 
         return card | chart.Value;
     }
