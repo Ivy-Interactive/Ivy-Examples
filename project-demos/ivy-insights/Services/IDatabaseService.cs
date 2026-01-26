91f98d36abb83fc06df76265fa1c8f571003d981
Artem <la.artem.ser@gmail.com>
2026-01-26 20:57:00 +0200

    chore: remove deprecated Ivy Insights project files including Docker configuration, database service, and application code

--- project-demos/ivy-insights/Services/IDatabaseService.cs
+++
@@ -1,9 +0,0 @@
-using IvyInsights.Models;
-
-namespace IvyInsights.Services;
-
-public interface IDatabaseService
-{
-    Task<List<DailyDownloadStats>> GetDailyDownloadStatsAsync(int days = 30, CancellationToken cancellationToken = default);
-}
-
--- project-demos/ivy-insights/Services/INuGetStatisticsProvider.cs
+++
@@ -1,9 +0,0 @@
-using IvyInsights.Models;
-
-namespace IvyInsights.Services;
-
-public interface INuGetStatisticsProvider
-{
-    Task<PackageStatistics> GetPackageStatisticsAsync(string packageId, CancellationToken cancellationToken = default);
-}
-
