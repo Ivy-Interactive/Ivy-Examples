5e37c32af7d515e03f264f7f22d9c644688a7202
Artem <la.artem.ser@gmail.com>
2026-03-15 23:32:48 +0200

    chore: remove a file.

--- project-demos/showcase-crm/.ivy/DatabaseGenerator/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System.Text.Json.Serialization;
-global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-namespace ShowcaseCrm;
