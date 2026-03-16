b0991ae0ac6686491c9cdc85ea89d8fa6f729481
Artem <la.artem.ser@gmail.com>
2026-03-16 21:30:37 +0200

    refactor(simmetrics-net): Reorganize variable declarations and enhance state management in SimMetricsNetApp

--- packages-demos/simmetrics-net/Apps/SimMetricsNetApp.cs
+++ packages-demos/simmetrics-net/Apps/SimMetricsNetApp.cs
@@ -5,11 +5,6 @@ public class SimMetricsNetApp : ViewBase
 {
     public override object? Build()
     {
-        List<NameSimilarity> CreateInitialNameList() =>
-            Enumerable.Range(1, 10)
-                .Select(_ => new NameSimilarity(new Faker().Name.FullName(), 0.0))
-                .ToList();
-
         var inputString = UseState(string.Empty);
         var inputMetric = UseState<SimMetricType?>(() => null);
         var shortDescription = UseState(string.Empty);
@@ -39,6 +34,13 @@ public class SimMetricsNetApp : ViewBase
 
             nameList.Set(results);
         }, inputString, inputMetric);
+
+        
+        List<NameSimilarity> CreateInitialNameList() =>
+            Enumerable.Range(1, 10)
+                .Select(_ => new NameSimilarity(new Faker().Name.FullName(), 0.0))
+                .ToList();
+
 
         var hasInput = !string.IsNullOrWhiteSpace(inputString.Value);
         var hasMetric = inputMetric.Value is SimMetricType;
